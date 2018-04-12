c09aeb4f71c9e2ebc06b5671377bd3bd8d948c60
John Rodriguez <john.rodriguez@gmail.com>
2018-04-12 15:25:26 -0500

    Move matrix/exif transforms into its own Transformer

--- picasso/src/main/java/com/squareup/picasso3/Request.java
+++ picasso/src/main/java/com/squareup/picasso3/Request.java
@@ -203,10 +203,6 @@ public final class Request {
     return targetWidth != 0 || targetHeight != 0;
   }
 
-  boolean needsTransformation() {
-    return needsMatrixTransform() || hasCustomTransformations();
-  }
-
   boolean needsMatrixTransform() {
     return hasSize() || rotationDegrees != 0;
   }
